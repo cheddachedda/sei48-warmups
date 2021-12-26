0c033d74611984a3d3216c9010996ca0b1aa7a2b
Daniel Nguyen <daniel.n.q.nguyen@gmail.com>
2021-12-26 19:51:47 +1100

    Solve Week01/Day3

--- Week01/Day3/index.js
+++ Week01/Day3/index.js
@@ -1,5 +1,14 @@
 const raindrops = function (n) {
-  // Write your code here
+  let output = '';
+
+  if (n !== 0) {
+    if (n % 3 === 0) output += 'Pling';
+    if (n % 5 === 0) output += 'Plang';
+    if (n % 7 === 0) output += 'Plong';
+  }
+  if (output === '') output += n;
+
+  return output;
 };
 
 // Required for tests; don't delete this.
